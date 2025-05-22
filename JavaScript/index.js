a287c2bedc8a73d8b8879228039f2f6538f2d443
Loicwr <nephtyse19@hotmail.fr>
2025-05-22 14:29:36 +0200

    fix (JavaScript): correct element IDs for player scores and add event listener for score change

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,11 +1,16 @@
 let buttonP1 = document.getElementById("buttonP1");
 let buttonP2 = document.getElementById("buttonP2");
 let buttonReset = document.getElementById("buttonReset");
-let scorePlayer1 = document.getElementById("scoreP1");
-let scorePlayer2 = document.getElementById("scoreP2");
+let scorePlayer1 = document.getElementById("scorePlayer1");
+let scorePlayer2 = document.getElementById("scorePlayer2");
 let scoreMaximun = document.getElementById("scoreMaximun");
 
 let scoreP1 = 0;
 let scoreP2 = 0;
 let scoreWin = parseInt(scoreMaximun.value);
 let gameOver = false;
+
+scoreMaximun.addEventListener("change",() => {
+    scoreWin = parseInt(scoreMaximun.value);
+    resetGame();
+})
