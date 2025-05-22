ac6dd8c95ec936189577f397d79940b802df6230
Loicwr <nephtyse19@hotmail.fr>
2025-05-22 14:21:49 +0200

    build (JavaScript): initialize score management variables and button elements

---
+++ JavaScript/index.js
@@ -0,0 +1,11 @@
+let buttonP1 = document.getElementById("buttonP1");
+let buttonP2 = document.getElementById("buttonP2");
+let buttonReset = document.getElementById("buttonReset");
+let scorePlayer1 = document.getElementById("scoreP1");
+let scorePlayer2 = document.getElementById("scoreP2");
+let scoreMaximun = document.getElementById("scoreMaximun");
+
+let scoreP1 = 0;
+let scoreP2 = 0;
+let scoreWin = parseInt(scoreMaximun.value);
+let gameOver = false;
